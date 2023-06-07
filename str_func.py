ebbe6bb3cced0a6d359f9c92a8beeae00df32a8e
guzel <guzel.kh23@gmail.ru>
2023-06-07 23:25:49 +0300

    add new function: str_title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(string: str):
     return string.upper
+
+
+def str_title(string: str):
+    """
+    делает заглавными первые буквы каждого слова в строке
+    :param string: str
+    :return: str
+    """
+    return string.title()
